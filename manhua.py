420646e08996e9ee13a8ac15cc4c20afb6725b83
11lookpig23 <mengjie@shanghaitech.edu.cn>
2017-09-03 23:03:22 +0800

    sdf

---
+++ manhua.py
@@ -0,0 +1,77 @@
+import urllib.request,os
+import re
+from bs4 import BeautifulSoup
+def get_each_comic_url(url):
+    #page=urllib.request.urlopen(url）headers={'User-Agent': 'Mozilla/5.0'})
+    page = urllib.request.Request(url,headers={'User-Agent': 'Mozilla/5.0'})
+
+    html=urllib.request.urlopen(page).read()
+    soup = BeautifulSoup(html,"html.parser")
+    body = soup.find_all(class_="preview-row")
+    reg = r'src="(http://[^>]+\.(?:jpeg|jpg))"'
+    urlre = re.compile(reg)
+    url_comcis = re.findall(urlre,str(body))
+    return url_comcis
+
+def get_url_list(http):
+    page = urllib.request.urlopen(http)
+    html = page.read()
+    soup = BeautifulSoup(html,"html.parser")
+    body = soup.find_all(class_="preview-row")
+    reg = r'href="\w*\.html"'
+    urlre = re.compile(reg)
+    urlist = re.findall(urlre,str(body))
+    return urlist
+
+'''
+#http = 'http://www.uuumh.com/shaonv/2016/0513/605.html'
+http = 'http://www.uuumh.com/shaonv/2016/1219/4118.html'
+urlist = get_url_list(http)
+i = 0
+'''
+os.mkdir('gouis')
+os.chdir(os.path.join(os.getcwd(), 'gouis'))
+'''
+for li in urlist:   
+    url = 'http://www.uuumh.com/shaonv/2016/1219/'+li[6:-1]  
+    try:
+        url_com = get_each_comic_url(url)[0]
+        urllib.request.urlretrieve(url_com, str(i)+'.jpg')
+        print("Success to download")
+    except:
+        print('get_error')
+
+    i+=1
+<div id="gallery-1" class="gallery galleryid-39327 gallery-columns-4 gallery-size-thumbnail"><dl class="gallery-item">
+<dt class="gallery-icon landscape">
+'''
+i = 0
+url_comcis = get_each_comic_url('https://svscomics.com/download/379497/glassfish-siterip-full-complete')
+for coms in url_comcis:
+    #print(coms)
+    
+    #try:
+    '''
+    page = urllib.request.Request(coms ,headers={'User-Agent': 'Mozilla/5.0'})
+    response = urllib.request.urlopen(page)
+    f = open('sjkg','wb')
+    f.write(response.read())
+    f.close()
+    print("Success to download")
+
+    #except:
+        #print('get_error')
+    '''
+    try:
+        opener=urllib.request.build_opener()
+        opener.addheaders=[('User-Agent','Mozilla/5.0')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(coms, str(i)+'.jpg') 
+        print("Success to download")
+    except:
+        print('get_error')
+    i+=1
+
+#<img src="http://img61.imagetwist.com/th/10818/5o6eltfcgo0e.jpg" title="Perfect  mother's natural big tits get fucked by son's cock in [Yanagawa Rio] Mamapai">
+
+#<div class="preview-row">
